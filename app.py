369e37027e8804af75fd7a97eb7dc4a901e3bfad
procrastinando <74340724+procrastinando@users.noreply.github.com>
2023-09-03 20:01:58 +1000

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import shutil
 import os
 from tortoise.api import TextToSpeech
 from tortoise.utils.audio import load_voices
+import random
 
 def rest_subs(end_sub, start_sub):
     end_sub = datetime.combine(datetime.today(), end_sub.to_time())
@@ -17,25 +18,25 @@ def rest_subs(end_sub, start_sub):
     return duration
 
 # gtts model
-# def t2v(text, name):
-#     tts = gTTS(text, lang='en')
-#     tts.save(f'results/tmp/{name}.2.wav')
+def t2v_gtts(text, name, extension):
+    tts = gTTS(text, lang='en')
+    tts.save(f'results/tmp/{name}.2.{extension}')
 
 # tortoise model
-def t2v(tts, emotion, text, voice_samples, custom_preset, name):
+def t2v_tortoise(tts, emotion, text, voice_samples, custom_preset, name, extension):
     gen = tts.tts_custom(emotion+text, voice_samples=voice_samples, custom_preset=custom_preset)
-    torchaudio.save(f'results/tmp/{name}.2.wav', gen.squeeze(0).cpu(), 24000)
+    torchaudio.save(f'results/tmp/{name}.2.{extension}', gen.squeeze(0).cpu(), 24000)
 
-def save_silent(duration, name):
-    # Compatible with gtts (replace all .wav to .wav)
-    # command = ["ffmpeg", "-f", "lavfi", "-i", "anullsrc=r=24000:cl=mono", "-t", str(duration), "-acodec", "libmp3lame", f"results/tmp/{name}.wav"]
-    # compatible with tortoise (replace all .wav to .wav)
-    command = ["ffmpeg", "-f", "lavfi", "-i", "anullsrc=channel_layout=mono:sample_rate=24000", "-t", str(duration), "-c:a", "pcm_f32le", f"results/tmp/{name}.wav"]
+def save_silent(duration, name, model, extension):
+    if model == 'gtts':
+        command = ["ffmpeg", "-f", "lavfi", "-i", "anullsrc=r=24000:cl=mono", "-t", str(duration), "-acodec", "libmp3lame", f"results/tmp/{name}.{extension}"]
+    if model == 'tortoise':
+        command = ["ffmpeg", "-f", "lavfi", "-i", "anullsrc=channel_layout=mono:sample_rate=24000", "-t", str(duration), "-c:a", "pcm_f32le", f"results/tmp/{name}.{extension}"]
     subprocess.run(command, capture_output=True)
     print(f"Silence: {duration}s")
 
-def shrink_audio(duration, name):
-    command = f"ffprobe -i results/tmp/{name}.2.wav -show_entries format=duration -v quiet -of csv=\"p=0\" -loglevel quiet"
+def shrink_audio(duration, name, model, extension):
+    command = f"ffprobe -i results/tmp/{name}.2.{extension} -show_entries format=duration -v quiet -of csv=\"p=0\" -loglevel quiet"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output_p = process.communicate()[0].decode("utf-8")
     duration1 = float(output_p.strip())
@@ -43,14 +44,14 @@ def shrink_audio(duration, name):
     if duration1 > duration:
         atempo = duration1 / duration
         print(f"Accelerate by {round(atempo, 2)}x")
-        command = f"ffmpeg -i results/tmp/{name}.2.wav -filter:a \"atempo={atempo}\" -vn -loglevel quiet results/tmp/{name}.2_.wav"
+        command = f"ffmpeg -i results/tmp/{name}.2.{extension} -filter:a \"atempo={atempo}\" -vn -loglevel quiet results/tmp/{name}.2_.{extension}"
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output_p = process.communicate()[0].decode("utf-8")
-        os.remove(f'results/tmp/{name}.2.wav')
-        os.rename(f'results/tmp/{name}.2_.wav', f'results/tmp/{name}.2.wav')
+        os.remove(f'results/tmp/{name}.2.{extension}')
+        os.rename(f'results/tmp/{name}.2_.{extension}', f'results/tmp/{name}.2.{extension}')
     else:
         atempo = duration1 / duration
-        save_silent(duration - duration1, f'{name}.3')
+        save_silent(duration - duration1, f'{name}.3', model, extension)
 
 def file_key(file):
     parts = file.split('.')
@@ -58,12 +59,20 @@ def file_key(file):
 
 ##########################################################################
 
+def voice_change(voice):
+    return gr.Audio.update(f"tortoise/voices/{voice}/" + random.choice(os.listdir(f"tortoise/voices/{voice}/")))
+
 def refresh_btn_click():
     global voices_list
     voices_list = os.listdir('tortoise/voices')
     return gr.Dropdown.update(choices=voices_list, value=voices_list[0])
 
-def generate_btn_click(subtitle, voice, emotion, num_autoregressive_samples, diffusion_iterations, temperature, length_penalty, repetition_penalty, top_p, cond_free, cond_free_k, diffusion_temperature, half):
+def generate_btn_click(subtitle, voice, emotion, model, num_autoregressive_samples, diffusion_iterations, temperature, length_penalty, repetition_penalty, top_p, cond_free, cond_free_k, diffusion_temperature, half):
+    # Clean temporary files
+    if os.path.exists("results/tmp/"):
+        shutil.rmtree("results/tmp/")
+    os.mkdir("results/tmp/")
+
     custom_preset = {
         'temperature': float(temperature),
         'length_penalty': float(length_penalty),
@@ -76,48 +85,50 @@ def generate_btn_click(subtitle, voice, emotion, num_autoregressive_samples, dif
         'cond_free': cond_free
         }
 
-    voice_samples, conditioning_latents = load_voices([voice])
-    tts = TextToSpeech(half=half)
-
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    with open(sub_path, "wb") as f:
+    with open("results/tmp/sub.srt", "wb") as f:
         f.write(subtitle)
-    subs = pysrt.open(sub_path)
+    subs = pysrt.open("results/tmp/sub.srt")
 
+    if model == 'gtts':
+        extension = 'mp3'
+    if model == 'tortoise':
+        extension = 'wav'
+        voice_samples, conditioning_latents = load_voices([voice])
+        tts = TextToSpeech(half=half)
+
+    # Syntetize audios
     start = pysrt.SubRipTime.from_ordinal(0)
     for number, sub in enumerate(subs):
         print(f" --> Processing subtitle {number}")
         duration = rest_subs(sub.start, start) # a duration
-        save_silent(duration, f'{number}.1')
-        t2v(tts, emotion, sub.text, voice_samples, custom_preset, number)
-        #t2v(sub.text, number)
+        save_silent(duration, f'{number}.1', model, extension)
+
+        if model == 'gtts':
+            t2v_gtts(sub.text, number, extension)
+        if model == 'tortoise':
+            t2v_tortoise(tts, emotion, sub.text, voice_samples, custom_preset, number, extension)
+
         duration = rest_subs(sub.end, sub.start) # audio duration
         start = sub.end
-        shrink_audio(duration, number)
+        shrink_audio(duration, number, model, extension)
     torch.cuda.empty_cache()
 
     # Get the list of audio files in the directory and sort them
-    files = [f for f in os.listdir(directory) if f.endswith(".wav")]
+    files = [f for f in os.listdir("results/tmp/") if f.endswith(f".{extension}")]
     files = sorted(files, key=file_key)
 
     # Create a command for ffmpeg to join the files
     command = ["ffmpeg", "-loglevel", "quiet", "-y"]
     for f in files:
-        command.extend(["-i", os.path.join(directory, f)])
-    command.extend(["-filter_complex", "concat=n={}:v=0:a=1".format(len(files)), output_path])
+        command.extend(["-i", os.path.join("results/tmp/", f)])
+    command.extend(["-filter_complex", "concat=n={}:v=0:a=1".format(len(files)), f"results/output.{extension}"])
     subprocess.run(command)
 
-    shutil.rmtree(directory)
-    return gr.Audio.update(output_path)
+    shutil.rmtree("results/tmp/")
+    return gr.Audio.update(f"results/output.{extension}")
 
 ##########################################################################
 
-directory = "results/tmp/"
-output_path = "results/output.wav"
-sub_path = "results/tmp/sub.srt"
-# list all the directories in 'tortoise/voices'
 voices_list = os.listdir('tortoise/voices')
 emotion_list = [None, '[I am really happy,] ', '[I am really sad,] ', '[I am really angry,] ', '[I am really disgusted,] ', '[Arrogant tone,] ', '[I am really surprised!,] ']
 
@@ -131,15 +142,16 @@ with gr.Blocks(title='ibarcena.net') as app:
 
     with gr.Row():
         with gr.Column():
+            model = gr.Dropdown(['gtts', 'tortoise'], value='tortoise', label='TTS model')
             subtitle = gr.File(type='binary', label='Subtitles file (.srt)')
             voice = gr.Dropdown(voices_list, value=voices_list[0], label='Voice')
             emotion = gr.Dropdown(emotion_list, value=emotion_list[0], label='Emotion')
+            preview = gr.Audio(f"tortoise/voices/{voice.value}/" + random.choice(os.listdir(f"tortoise/voices/{voice.value}/")), label='Preview')
             refresh_btn = gr.Button('Refresh')
         with gr.Column():
             num_autoregressive_samples = gr.Slider(minimum=4, maximum=512, value=4, label='Autoregressive samples', step=1)
             diffusion_iterations = gr.Slider(minimum=4, maximum=512, value=80, label='Diffusion iterations', step=1)
             temperature = gr.Slider(minimum=0.1, maximum=1, value=0.8, label='Temperature')
-        with gr.Column():
             with gr.Row():
                 half = gr.Checkbox(label='Half precision', value=True)
                 cond_free = gr.Checkbox(label='Conditional free', value=True)
@@ -153,5 +165,6 @@ with gr.Blocks(title='ibarcena.net') as app:
             output = gr.Audio(label='Output')
 
     refresh_btn.click(refresh_btn_click, outputs=[voice])
-    generate_btn.click(generate_btn_click, [subtitle, voice, emotion, num_autoregressive_samples, diffusion_iterations, temperature, length_penalty, repetition_penalty, top_p, cond_free, cond_free_k, diffusion_temperature, half], outputs=[output])
+    voice.change(voice_change, inputs=[voice], outputs=[preview])
+    generate_btn.click(generate_btn_click, [subtitle, voice, emotion, model, num_autoregressive_samples, diffusion_iterations, temperature, length_penalty, repetition_penalty, top_p, cond_free, cond_free_k, diffusion_temperature, half], outputs=[output])
     app.launch(share=False, debug=True)
